e5fb217e290fbd435461a0f32de374b2bb3729a8
Akshay Nair <phenax5@gmail.com>
2018-11-23 22:02:42 +0530

    Removes build directory from .gitignore for gh-pages

---
+++ build/precache-manifest.05cc5d3da28bc74a9f919bc28ef7744e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "542228a2ab6020d8ee88",
+    "url": "/static/js/main.542228a2.chunk.js"
+  },
+  {
+    "revision": "b7640d569c1d4a7c96ef",
+    "url": "/static/js/1.b7640d56.chunk.js"
+  },
+  {
+    "revision": "542228a2ab6020d8ee88",
+    "url": "/static/css/main.7a6baf39.chunk.css"
+  },
+  {
+    "revision": "f3f1e3c764f1a829e3ba5783e4ece1c6",
+    "url": "/index.html"
+  }
+];
