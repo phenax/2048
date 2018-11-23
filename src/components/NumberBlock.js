5d7cb1c9d967d0266042166e6b4c58a67c4c38c7
Akshay Nair <phenax5@gmail.com>
2018-11-23 22:51:27 +0530

    Refactors some more

--- src/components/NumberBlock.js
+++ src/components/NumberBlock.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Rect, Text } from './Canvas';
-import { getColor } from '../utils/block-utils';
+import { getColor, isZero } from '../utils/block-utils';
 
 const NumberBlock = ({ block, size, x, y }) => {
   const bounds = { x, y, width: size, height: size };
   return (
     <React.Fragment>
       <Rect {...bounds} fill={getColor(block.number)} />
-      {block.number !== 0 && <Text
+      {!isZero(block) && <Text
         {...bounds}
         text={block.number}
         color="#333"
--- src/utils/block-utils.js
+++ src/utils/block-utils.js
@@ -9,33 +9,31 @@ export const Block = number => ({ number });
 // zero :: () -> Block
 export const zero = () => Block(0);
 
+// isZero :: Block -> Boolean
+export const isZero = block => block.number === 0;
+
 // getColor :: Number -> String
 export const getColor = number => number === 0
   ? 'rgb(240, 240, 240)'
   : `hsl(${[
     `${150 + (10 * (Math.log2(number) % 10))}`,
-    '60%',
+    '50%',
     '58%',
   ].join(',')})`;
 
+// mapIndexed :: ((a, Number) -> b) -> [a] -> [b]
 const mapIndexed = createPipe('map');
-
-// getNumber :: Block -> Number
-const getNumber = x => x.number;
-// getFirstNumber :: [Block] -> Number
-const getFirstNumber = compose(getNumber, head);
-// getLastNumber :: [Block] -> Number
-const getLastNumber = compose(getNumber, last);
 
 // sumMatches :: FlowDirection -> [Block] -> [Block]
 export const sumMatches = direction => compose(
   map(Block),
   chain(xs => {
+    const { number = 0 } = head(xs) || {};
     const groups = range(0, Math.floor(xs.length / 2))
-      .map(x => 2 * getFirstNumber(xs));
+      .map(x => 2 * number);
 
     return xs.length % 2
-      ? FlowDirection.concatArray(groups, [getFirstNumber(xs)])(direction)
+      ? FlowDirection.concatArray(groups, [number])(direction)
       : groups;
   }),
   groupWith(eqProps('number')),
@@ -44,15 +42,15 @@ export const sumMatches = direction => compose(
 // padRow :: (Number, FlowDirection, () -> Block) -> [Block] -> [Block]
 export const padRow = (size, direction, getBlock) => row => {
   const paddingLen = size - row.length;
-  if(paddingLen === 0) return row;
+  if(paddingLen <= 0) return row;
   const padding = range(0, paddingLen).map(getBlock);
   return FlowDirection.concatArray(row, padding)(direction);
 };
 
 // addBlock :: ([Block], FlowDirection) -> ([Block], index) -> [Block]
 export const addBlock = (newRow, direction) => (row, index) => FlowDirection.match(direction, {
-  Forward: () => getLastNumber(row) === 0 ? [ ...row.slice(0, row.length - 1), newRow[index] ] : row,
-  Reverse: () => getFirstNumber(row) === 0 ? [ newRow[index], ...row.slice(1, row.length) ] : row,
+  Forward: () => isZero(last(row)) ? [ ...row.slice(0, row.length - 1), newRow[index] ] : row,
+  Reverse: () => isZero(head(row)) ? [ newRow[index], ...row.slice(1, row.length) ] : row,
 });
 
 // moveHorizontal :: (Number, FlowDirection, [Block]) -> [[Block]] -> [[Block]]
@@ -60,7 +58,7 @@ export const moveHorizontal = (size, direction, newBlock) => compose(
   mapIndexed(addBlock(newBlock, direction)),
   map(padRow(size, direction, zero)),
   map(sumMatches(direction)),
-  map(filter(item => getNumber(item) !== 0)),
+  map(filter(isZero)),
 );
 
 // moveVertical :: (Number, FlowDirection, [Block]) -> [[Block]] -> [[Block]]
